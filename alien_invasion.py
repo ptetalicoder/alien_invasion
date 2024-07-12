0f7ae0a14e5947bcf86111ff691d3d08ad048120
Pranav Tetali <ptetali0801@gmail.com>
2024-07-11 19:54:26 -0500

    created settings file and imbeded it to ai file

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from settings import Settings
 
 
 class AlienInvasion:
@@ -8,8 +9,12 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.settings =Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
         pygame.display.set_caption("Alien Invasion")
 
         # Set the background color.
@@ -24,7 +29,7 @@ class AlienInvasion:
                     sys.exit()
 
             # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
