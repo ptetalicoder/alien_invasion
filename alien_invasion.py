4f65d8b25edc4b105ce23ae1c71d971a37c26528
Pranav Tetali <ptetali0801@gmail.com>
2024-07-15 19:34:36 -0500

    refactoring, need to work on spacing

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,9 +27,31 @@ class AlienInvasion:
         """Start the main loop of the game."""
         while True:
             # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+           
+
+    def _check_events(self):
+        """Respond to key press and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                     sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                            self.ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+
+            elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                           self.ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                  self.ship.moving_left = False
+
+    def _update_screen(self):
+            """Update images on the screen, and flip to the new screen."""
 
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
@@ -42,33 +64,6 @@ if __name__ == '__main__':
     # Make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
