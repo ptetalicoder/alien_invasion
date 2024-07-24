d4906b09f59a1e19d5f44b719183fa49b998fb81
Pranav Tetali <ptetali0801@gmail.com>
2024-07-23 22:43:57 -0500

    added row of aliens in A_I and created create_fleet method

--- alien.py
+++ alien.py
@@ -7,7 +7,7 @@ from pygame.sprite import Sprite
 class Alien(Sprite):
     """A class to represent a single alien in the fleet"""
 
-    def __init__(self, ai_agame):
+    def __init__(self, ai_game):
         """Initialize the alien and set its starting position."""
         super().__init__()
         self.screen = ai_game.screen
