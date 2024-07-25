b7e3332a8c43b591d090c2d7cd2e52bed5ae319d
Pranav Tetali <ptetali0801@gmail.com>
2024-07-25 17:03:17 -0500

    Created game stats file and made sure shiop gets hit by alien.

--- alien_invasion.py
+++ alien_invasion.py
@@ -132,6 +132,10 @@ class AlienInvasion:
         self._check_fleet_edges()
         self.aliens.update()
 
+        # Look for alien-ship collisions.
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            print("Ship hit!!!")
+
     def _check_fleet_edges(self):
         """Respond appropriately if any aliens have reched an edge."""
         for alien in self.aliens.sprites():
